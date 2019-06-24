fea61c3a0b9cc35130ac9c4d18f71b876aa80814
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-06-25 00:06:55 +0100

    Update App.kt

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,44 +1,4 @@
-import auths.AuthManager
 import commandline.LabPipeServerCommandLine
-import configs.LabPipeConfig
-import db.DatabaseConnector
-import picocli.CommandLine
-import services.DevService
-import services.GeneralService
-import services.ParameterService
-import services.RecordService
-import java.nio.file.Paths
-import io.javalin.Javalin
-
-
-class App : Runnable {
-
-    override fun run() {
-        val config = LabPipeConfig(systemTemporaryDirectory)
-        config.dbHost = dbHost
-        config.dbPort = dbPort
-        config.dbName = dbName
-        config.dbUser = dbUser
-        config.dbPass = dbPass
-        config.emailHost = emailHost
-        config.emailPort = emailPort
-        config.emailUser = emailUser
-        config.emailPass = emailPass
-
-        DatabaseConnector.connect(config)
-        if (actionTest) {
-//            loadConfigurations()
-        } else if (actionRun) {
-            val app = Javalin.create()
-            AuthManager.setManager(app)
-            GeneralService.routes(app)
-            ParameterService.routes(app)
-            RecordService.routes(app)
-            DevService.routes(app)
-            app.start(4567)
-        }
-    }
-}
 
 
 fun main(args: Array<String>) {
