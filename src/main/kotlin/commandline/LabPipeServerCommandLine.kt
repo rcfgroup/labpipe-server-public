913a0e9248d5cecf7d0b69bc1b4289070ed9402e
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-06-26 12:45:26 +0100

    Update LabPipeServerCommandLine.kt

--- src/main/kotlin/commandline/LabPipeServerCommandLine.kt
+++ src/main/kotlin/commandline/LabPipeServerCommandLine.kt
@@ -3,25 +3,23 @@ package commandline
 import auths.AuthManager
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.output.TermUi.echo
+import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.choice
 import com.github.ajalt.clikt.parameters.types.int
 import configs.LabPipeConfig
 import db.DatabaseConnector
-import io.javalin.Javalin
 import org.apache.commons.configuration2.PropertiesConfiguration
 import org.apache.commons.configuration2.builder.fluent.Configurations
 import org.apache.commons.configuration2.ex.ConfigurationException
-import services.DevService
-import services.GeneralService
-import services.ParameterService
-import services.RecordService
+import services.*
+import sessions.BuiltInValue
 import sessions.InMemoryData
 import java.io.File
 import java.nio.file.Paths
 
 fun updateConfig(path:String? = null, key: String, value: String?) {
-    val configFilePath = path ?: "config.ini"
+    val configFilePath = path ?: BuiltInValue.DEFAULT_CONFIG_FILE_NAME
     val configFile = File(configFilePath)
     configFile.createNewFile()
     val configs = Configurations();
@@ -41,7 +39,7 @@ fun updateConfig(path:String? = null, key: String, value: String?) {
 }
 
 fun updateConfig(path:String? = null, key: String, value: Int?) {
-    val configFilePath = path ?: "config.ini"
+    val configFilePath = path ?: BuiltInValue.DEFAULT_CONFIG_FILE_NAME
     val configFile = File(configFilePath)
     configFile.createNewFile()
     val configs = Configurations();
@@ -61,32 +59,34 @@ fun updateConfig(path:String? = null, key: String, value: Int?) {
 }
 
 fun readConfig(path: String? = null): PropertiesConfiguration? {
-    val configFilePath = path ?: "config.ini"
+    val configFilePath = path ?: BuiltInValue.DEFAULT_CONFIG_FILE_NAME
     val configFile = File(configFilePath)
     val configs = Configurations()
-    if (!configFile.exists()) {
-        updateConfig(path = path, key = "database.host", value = "localhost")
-        updateConfig(path = path, key = "database.port", value = 27017)
-        updateConfig(path = path, key = "database.name", value = "labpipe-dev")
+    when {
+        !configFile.exists() -> {
+            updateConfig(path = path, key = BuiltInValue.PROPERTIES_FIELD_DB_HOST, value = "localhost")
+            updateConfig(path = path, key = BuiltInValue.PROPERTIES_FIELD_DB_PORT, value = 27017)
+            updateConfig(path = path, key = BuiltInValue.PROPERTIES_FIELD_DB_NAME, value = "labpipe-dev")
 
-        updateConfig(path = path, key = "mail.host", value = "localhost")
-        updateConfig(path = path, key = "mail.port", value = 25)
+            updateConfig(path = path, key = BuiltInValue.PROPERTIES_FIELD_EMAIL_HOST, value = "localhost")
+            updateConfig(path = path, key = BuiltInValue.PROPERTIES_FIELD_EMAIL_PORT, value = 25)
 
-        val defaultCacheDir = Paths.get(System.getProperty("user.home"), "labpipe").toString()
-        updateConfig(path = path, key = "path.cache", value = defaultCacheDir)
+            val defaultCacheDir = Paths.get(System.getProperty("user.home"), "labpipe").toString()
+            updateConfig(path = path, key = BuiltInValue.PROPERTIES_FIELD_PATH_CACHE, value = defaultCacheDir)
 
-        echo("Config file not found at: [$path]")
-        echo("Created config file at: [${configFile.absolutePath}]")
-        echo("Default settings:")
-        echo("------ Database ------")
-        echo("[HOST]: localhost")
-        echo("[PORT]: 27017")
-        echo("[NAME]: labpipe-dev")
-        echo("------ Email Server ------")
-        echo("[HOST]: localhost")
-        echo("[PORT]: 25")
-        echo("------ Cache Directory ------")
-        echo("[CACHE]: $defaultCacheDir")
+            echo("Config file not found at: [$path]")
+            echo("Created config file at: [${configFile.absolutePath}]")
+            echo("Default settings:")
+            echo("------ Database ------")
+            echo("[HOST]: localhost")
+            echo("[PORT]: 27017")
+            echo("[NAME]: labpipe-dev")
+            echo("------ Email Server ------")
+            echo("[HOST]: localhost")
+            echo("[PORT]: 25")
+            echo("------ Cache Directory ------")
+            echo("[CACHE]: $defaultCacheDir")
+        }
     }
     return try
     {
@@ -99,37 +99,67 @@ fun readConfig(path: String? = null): PropertiesConfiguration? {
     }
 }
 
-fun checkConfig(configPath: String?) {
+fun importConfig(configPath: String?) {
     val properties = readConfig(configPath)
-    val config = LabPipeConfig(properties?.getString("path.cache") ?: Paths.get(System.getProperty("user.home"), "labpipe").toString())
-    config.dbHost = properties?.getString("database.host") ?: "localhost"
-    config.dbPort = properties?.getInt("database.port") ?: 27017
-    config.dbName = properties?.getString("database.name") ?: "labpipe-dev"
-    config.dbUser = properties?.getString("database.user")
-    config.dbPass = properties?.getString("database.pass")
-    config.emailHost = properties?.getString("mail.host") ?: "localhost"
-    config.emailPort = properties?.getInt("mail.port") ?: 25
-    config.emailUser = properties?.getString("mail.user")
-    config.emailPass = properties?.getString("mail.pass")
-    InMemoryData.labPipeConfig = config
+    properties?.run {
+        InMemoryData.labPipeConfig = LabPipeConfig(properties.getString(BuiltInValue.PROPERTIES_FIELD_PATH_CACHE) ?: Paths.get(System.getProperty("user.home"), "labpipe").toString())
+        InMemoryData.labPipeConfig.dbHost = when {
+            properties.containsKey(BuiltInValue.PROPERTIES_FIELD_DB_HOST) -> properties.getString(BuiltInValue.PROPERTIES_FIELD_DB_HOST)
+            else -> "localhost"
+        }
+        InMemoryData.labPipeConfig.dbPort = when {
+            properties.containsKey(BuiltInValue.PROPERTIES_FIELD_DB_PORT) -> properties.getInt(BuiltInValue.PROPERTIES_FIELD_DB_PORT)
+            else -> 27017
+        }
+        InMemoryData.labPipeConfig.dbName = when {
+            properties.containsKey(BuiltInValue.PROPERTIES_FIELD_DB_NAME) -> properties.getString(BuiltInValue.PROPERTIES_FIELD_DB_NAME)
+            else -> "labpipe-dev"
+        }
+        InMemoryData.labPipeConfig.dbUser = when {
+            properties.containsKey(BuiltInValue.PROPERTIES_FIELD_DB_USER) -> properties.getString(BuiltInValue.PROPERTIES_FIELD_DB_USER)
+            else -> null
+        }
+        InMemoryData.labPipeConfig.dbPass = when {
+            properties.containsKey(BuiltInValue.PROPERTIES_FIELD_DB_PASS) -> properties.getString(BuiltInValue.PROPERTIES_FIELD_DB_PASS)
+            else -> null
+        }
+        InMemoryData.labPipeConfig.emailHost = when {
+            properties.containsKey(BuiltInValue.PROPERTIES_FIELD_EMAIL_HOST) -> properties.getString(BuiltInValue.PROPERTIES_FIELD_EMAIL_HOST)
+            else -> "localhost"
+        }
+        InMemoryData.labPipeConfig.emailPort = when {
+            properties.containsKey(BuiltInValue.PROPERTIES_FIELD_EMAIL_PORT) -> properties.getInt(BuiltInValue.PROPERTIES_FIELD_EMAIL_PORT)
+            else -> 25
+        }
+        InMemoryData.labPipeConfig.emailUser = when {
+            properties.containsKey(BuiltInValue.PROPERTIES_FIELD_EMAIL_USER) -> properties.getString(BuiltInValue.PROPERTIES_FIELD_EMAIL_USER)
+            else -> null
+        }
+        InMemoryData.labPipeConfig.emailPass = when {
+            properties.containsKey(BuiltInValue.PROPERTIES_FIELD_EMAIL_PASS) -> properties.getString(BuiltInValue.PROPERTIES_FIELD_EMAIL_PASS)
+            else -> null
+        }
+    }
 }
 
-fun startServer(configPath: String?) {
-    checkConfig(configPath)
-    DatabaseConnector.connect(InMemoryData.labPipeConfig)
-    val app = Javalin.create()
-    AuthManager.setManager(app)
-    GeneralService.routes(app)
-    ParameterService.routes(app)
-    RecordService.routes(app)
-    DevService.routes(app)
-    app.start(4567)
+fun startServer(port: Int) {
+    DatabaseConnector.connect()
+    AuthManager.setManager()
+    GeneralService.routes()
+    ParameterService.routes()
+    RecordService.routes()
+    FormService.routes()
+    DevService.routes()
+    InMemoryData.labPipeServer.start(port)
 }
 
 class LabPipeServerCommandLine : CliktCommand() {
+
     val action by option("--action", help = "server actions").choice("start", "stop", "restart")
 
     val configPath by option("--config", help = "config file path")
+
+    val serverPort by option("--port", help = "server port").int().default(4567)
 
     val dbHost by option("--db-host", help = "database host")
     val dbPort by option("--db-port", help = "database port").int()
@@ -145,21 +175,24 @@ class LabPipeServerCommandLine : CliktCommand() {
     val cacheDir by option("--cache-dir", help = "cache directory")
 
     override fun run() {
-        updateConfig(path = configPath, key = "database.host", value = dbHost)
-        updateConfig(path = configPath, key = "database.port", value = dbPort)
-        updateConfig(path = configPath, key = "database.name", value = dbName)
-        updateConfig(path = configPath, key = "database.user", value = dbUser)
-        updateConfig(path = configPath, key = "database.pass", value = dbPass)
+        updateConfig(path = configPath, key = BuiltInValue.PROPERTIES_FIELD_DB_HOST, value = dbHost)
+        updateConfig(path = configPath, key = BuiltInValue.PROPERTIES_FIELD_DB_PORT, value = dbPort)
+        updateConfig(path = configPath, key = BuiltInValue.PROPERTIES_FIELD_DB_NAME, value = dbName)
+        updateConfig(path = configPath, key = BuiltInValue.PROPERTIES_FIELD_DB_USER, value = dbUser)
+        updateConfig(path = configPath, key = BuiltInValue.PROPERTIES_FIELD_DB_PASS, value = dbPass)
 
-        updateConfig(path = configPath, key = "mail.host", value = emailHost)
-        updateConfig(path = configPath, key = "mail.port", value = emailPort)
-        updateConfig(path = configPath, key = "mail.user", value = emailUser)
-        updateConfig(path = configPath, key = "mail.pass", value = emailPass)
+        updateConfig(path = configPath, key = BuiltInValue.PROPERTIES_FIELD_EMAIL_HOST, value = emailHost)
+        updateConfig(path = configPath, key = BuiltInValue.PROPERTIES_FIELD_EMAIL_PORT, value = emailPort)
+        updateConfig(path = configPath, key = BuiltInValue.PROPERTIES_FIELD_EMAIL_USER, value = emailUser)
+        updateConfig(path = configPath, key = BuiltInValue.PROPERTIES_FIELD_EMAIL_PASS, value = emailPass)
 
-        updateConfig(path = configPath, key = "path.cache", value = cacheDir)
+        updateConfig(path = configPath, key = BuiltInValue.PROPERTIES_FIELD_PATH_CACHE, value = cacheDir)
 
         when(action) {
-            "start" -> startServer(configPath)
+            "start" -> {
+                importConfig(configPath)
+                startServer(serverPort)
+            }
             else -> echo(action)
         }
     }
