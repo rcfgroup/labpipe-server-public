1d809764ba8413e79472e629aab8e32d190f2b3a
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-06-26 12:45:53 +0100

    Update NotificationUtil.kt

--- src/main/kotlin/notification/NotificationUtil.kt
+++ src/main/kotlin/notification/NotificationUtil.kt
@@ -3,6 +3,7 @@ package notification
 import com.google.gson.JsonElement
 import com.mongodb.client.model.Filters.eq
 import db.DatabaseConnector
+import sessions.InMemoryData
 
 object NotificationUtil {
     fun single(jsonElement: JsonElement) {
@@ -10,7 +11,7 @@ object NotificationUtil {
             var jsonObject = jsonElement.asJsonObject
             var toUser = jsonObject.get("to").asString
             var message = jsonObject.get("message").asJsonObject
-            var operators = DatabaseConnector.database.getCollection("parameters").find(eq("param_name", "operators"))
+            var operators = InMemoryData.mongoDatabase.getCollection("parameters").find(eq("param_name", "operators"))
         }
     }
 }
