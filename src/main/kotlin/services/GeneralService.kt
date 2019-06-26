a799379138286d0f140265e493be019e697dc4b4
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-06-26 12:45:45 +0100

    Update GeneralService.kt

--- src/main/kotlin/services/GeneralService.kt
+++ src/main/kotlin/services/GeneralService.kt
@@ -3,12 +3,13 @@ package services
 import auths.AuthManager
 import io.javalin.Javalin
 import io.javalin.core.security.SecurityUtil.roles
+import sessions.InMemoryData
 
 object GeneralService {
-    fun routes(app: Javalin) {
+    fun routes() {
         println("Add general service routes.")
-        app.get("/api/general/connect", { ctx -> ctx.result("API server connected successfully.") }, roles(AuthManager.ApiRole.PUBLIC))
-        app.get("/api/general/connect-with-auth", { ctx -> ctx.result("API server connected successfully.") }, roles(AuthManager.ApiRole.AUTHORISED))
-        app.get("/api/general/connect-with-token", { ctx -> ctx.result("API server connected successfully.") }, roles(AuthManager.ApiRole.TOKEN_AUTHORISED))
+        InMemoryData.labPipeServer.get("/api/general/connect", { ctx -> ctx.result("API server connected successfully.") }, roles(AuthManager.ApiRole.PUBLIC))
+        InMemoryData.labPipeServer.get("/api/general/connect-with-auth", { ctx -> ctx.result("API server connected successfully.") }, roles(AuthManager.ApiRole.AUTHORISED))
+        InMemoryData.labPipeServer.get("/api/general/connect-with-token", { ctx -> ctx.result("API server connected successfully.") }, roles(AuthManager.ApiRole.TOKEN_AUTHORISED))
     }
 }
