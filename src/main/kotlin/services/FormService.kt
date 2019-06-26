48eb1f7cb97a1a68c472ae3d627f73aacb0bbbbe
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-06-26 12:45:48 +0100

    Update FormService.kt

--- src/main/kotlin/services/FormService.kt
+++ src/main/kotlin/services/FormService.kt
@@ -8,19 +8,19 @@ import io.javalin.core.security.SecurityUtil
 import org.litote.kmongo.eq
 import org.litote.kmongo.findOne
 import org.litote.kmongo.getCollection
+import sessions.InMemoryData
 
 object FormService {
-    fun routes(app: Javalin) {
-        app.get("/api/form/template/:formCode", { ctx -> getFormTemplate(ctx.pathParam("formCode"))?.let { ctx.json(it) } },
+    fun routes() {
+        println("Add form service routes")
+        InMemoryData.labPipeServer.get("/api/form/template/:formCode", { ctx -> getFormTemplate(ctx.pathParam("formCode"))?.let { ctx.json(it) } },
             SecurityUtil.roles(AuthManager.ApiRole.PUBLIC, AuthManager.ApiRole.AUTHORISED)
         )
     }
 
 
     fun getFormTemplate(formCode: String): ParamFormTemplate? {
-        val col = DatabaseConnector.database.getCollection<ParamFormTemplate>("FORM_TEMPLATES")
-        println(col.countDocuments())
-        println(col.find().first())
+        val col = InMemoryData.mongoDatabase.getCollection<ParamFormTemplate>("FORM_TEMPLATES")
         return col.findOne(ParamFormTemplate::code eq formCode)
     }
 }
