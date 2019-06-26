c8a3290856f2276820bf142c2fb33c95b95bf797
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-06-26 12:45:43 +0100

    Update ParameterService.kt

--- src/main/kotlin/services/ParameterService.kt
+++ src/main/kotlin/services/ParameterService.kt
@@ -5,20 +5,21 @@ import db.DatabaseConnector
 import io.javalin.Javalin
 import io.javalin.core.security.SecurityUtil.roles
 import org.litote.kmongo.*
+import sessions.InMemoryData
 
 object ParameterService {
 
     private fun getWithParameterName(paramName: String): List<Any> {
         println("Getting parameter [$paramName]")
-        val col = DatabaseConnector.database.getCollection(paramName)
-        return col.aggregate<Any>(
-            project(excludeId())
-        ).toList()
+        val col = InMemoryData.mongoDatabase.getCollection(paramName)
+        return listOf(
+            col.aggregate<Any>(project(excludeId()))
+        )
     }
 
-    fun routes(app: Javalin) {
+    fun routes() {
         println("Add parameter service routes.")
-        app.get("/api/parameter/name/:paramName", { ctx -> ctx.json(getWithParameterName(ctx.pathParam("paramName"))) },
+        InMemoryData.labPipeServer.get("/api/parameter/name/:paramName", { ctx -> ctx.json(getWithParameterName(ctx.pathParam("paramName"))) },
             roles(AuthManager.ApiRole.AUTHORISED, AuthManager.ApiRole.TOKEN_AUTHORISED)
         )
     }
