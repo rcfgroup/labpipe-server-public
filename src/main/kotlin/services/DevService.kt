843ef32ffb569ce523a6b06f0b56ac8a861e0926
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-06-26 12:45:50 +0100

    Update DevService.kt

--- src/main/kotlin/services/DevService.kt
+++ src/main/kotlin/services/DevService.kt
@@ -8,22 +8,21 @@ import io.javalin.core.security.SecurityUtil.roles
 import kotlinx.html.*
 import kotlinx.html.stream.appendHTML
 import org.litote.kmongo.*
+import sessions.InMemoryData
 
 object DevService {
-    fun routes(app: Javalin) {
+    fun routes() {
         println("Loading developer service routes.")
-        app.get("/api/dev/html", { ctx -> ctx.html(testReplace()) },
+        InMemoryData.labPipeServer.get("/api/dev/html", { ctx -> ctx.html(testReplace()) },
             roles(AuthManager.ApiRole.PUBLIC, AuthManager.ApiRole.AUTHORISED)
         )
-        app.get("/api/dev/form-template", { ctx -> ctx.json(testFormTemplate()) },
+        InMemoryData.labPipeServer.get("/api/dev/form-template", { ctx -> ctx.json(testFormTemplate()) },
             roles(AuthManager.ApiRole.PUBLIC, AuthManager.ApiRole.AUTHORISED)
         )
     }
 
     fun testFormTemplate(): ParamFormTemplate {
-        val col = DatabaseConnector.database.getCollection<ParamFormTemplate>("FORM_TEMPLATES")
-        println(col.countDocuments())
-        println(col.find().first())
+        val col = InMemoryData.mongoDatabase.getCollection<ParamFormTemplate>("FORM_TEMPLATES")
         return col.find().first() ?: ParamFormTemplate("no_found", "Template Not Found")
     }
 
