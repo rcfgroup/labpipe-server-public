795a8dbbd49bafbf8a289d05659a9e577741a07d
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-06-26 12:45:40 +0100

    Update RecordService.kt

--- src/main/kotlin/services/RecordService.kt
+++ src/main/kotlin/services/RecordService.kt
@@ -8,6 +8,7 @@ import db.DatabaseConnector
 import io.javalin.Javalin
 import io.javalin.core.security.SecurityUtil
 import org.bson.Document
+import sessions.InMemoryData
 import java.util.*
 
 object RecordService {
@@ -31,7 +32,7 @@ object RecordService {
         val formCode = jsonObject.get("form_code").asString
         val record = Document.parse(Gson().toJson(jsonElement))
         try {
-            val collection = DatabaseConnector.database
+            val collection = InMemoryData.mongoDatabase
                 .getCollection("UPLOADED_FORM_DATA_$formCode")
             collection.insertOne(record)
         } catch (e: Exception) {
@@ -41,12 +42,14 @@ object RecordService {
         return "[Form: $formCode] data is saved."
     }
 
-    fun routes(app: Javalin) {
+    fun routes() {
         println("Add record service routes.")
-        app.post("/api/record/add", { ctx ->
+        InMemoryData.labPipeServer.post("/api/record/add", { ctx ->
+            val operator = AuthManager.getUser(ctx)
             val jsonParser = JsonParser()
             val jsonElement = jsonParser.parse(ctx.body())
             println(ctx.body())
+
             saveRecord(jsonElement)
         }, SecurityUtil.roles(AuthManager.ApiRole.AUTHORISED))
     }
