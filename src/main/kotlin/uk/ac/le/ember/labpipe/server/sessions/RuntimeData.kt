2fd4ac114f594bc24ae747193e5299b612d710c5
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-07-02 17:04:57 +0100

    Update RuntimeData.kt

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/sessions/RuntimeData.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/sessions/RuntimeData.kt
@@ -2,8 +2,9 @@ package uk.ac.le.ember.labpipe.server.sessions
 
 import com.mongodb.MongoClient
 import com.mongodb.client.MongoDatabase
-import uk.ac.le.ember.labpipe.server.configs.LPConfig
 import io.javalin.Javalin
+import org.simplejavamail.mailer.Mailer
+import uk.ac.le.ember.labpipe.server.data.LPConfig
 
 class RuntimeData {
     companion object {
@@ -12,5 +13,6 @@ class RuntimeData {
 
         lateinit var mongoClient: MongoClient
         lateinit var mongoDatabase: MongoDatabase
+        lateinit var mailer: Mailer
     }
 }
