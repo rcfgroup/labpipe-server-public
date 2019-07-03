7b894b0681e56e3f12971362047935301b60ccc6
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-07-03 16:26:33 +0100

    Update EmailUtil.kt

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/notification/EmailUtil.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/notification/EmailUtil.kt
@@ -5,18 +5,33 @@ import org.simplejavamail.email.EmailBuilder
 import org.simplejavamail.email.Recipient
 import org.simplejavamail.mailer.Mailer
 import org.simplejavamail.mailer.MailerBuilder
-import uk.ac.le.ember.labpipe.server.sessions.RuntimeData
+import uk.ac.le.ember.labpipe.server.sessions.Runtime
 import javax.activation.FileDataSource
 
-data class EmailAttachment (var name: String, var file: FileDataSource)
+data class EmailAttachment(var name: String, var file: FileDataSource)
 
 object EmailUtil {
     fun connect(): Mailer {
-        RuntimeData.mailer = MailerBuilder.withSMTPServerHost(RuntimeData.labPipeConfig.emailHost)
-            .withSMTPServerPort(RuntimeData.labPipeConfig.emailPort)
-            .withSMTPServerUsername(RuntimeData.labPipeConfig.emailUser)
-            .withSMTPServerPassword(RuntimeData.labPipeConfig.emailPass).buildMailer()
-        return RuntimeData.mailer
+        Runtime.mailer = MailerBuilder.withSMTPServerHost(Runtime.config.emailHost)
+            .withSMTPServerPort(Runtime.config.emailPort)
+            .withSMTPServerUsername(Runtime.config.emailUser)
+            .withSMTPServerPassword(Runtime.config.emailPass).buildMailer()
+        return Runtime.mailer
+    }
+
+    fun testConnection(): Boolean {
+        return try {
+            Runtime.mailer.testConnection()
+            Runtime.logger.info { "Email server connection successful." }
+            true
+        } catch (e: Exception) {
+            if (Runtime.config.debugMode) {
+                Runtime.logger.error(e) { "Cannot connect to email server." }
+            } else {
+                Runtime.logger.error { "Cannot connect to email server." }
+            }
+            false
+        }
     }
 
     fun sendEmail(
@@ -28,14 +43,19 @@ object EmailUtil {
         attachments: List<EmailAttachment> = mutableListOf(),
         async: Boolean
     ) {
-        val email = EmailBuilder.startingBlank()
-            .from(from)
-            .to(to)
-            .withSubject(subject)
-            .withPlainText(text)
-            .withHTMLText(html)
-            .withAttachments(attachments.map { x -> AttachmentResource(x.name, x.file) })
-            .buildEmail()
-        RuntimeData.mailer.sendMail(email, async)
+        try {
+            Runtime.mailer.testConnection()
+            val email = EmailBuilder.startingBlank()
+                .from(from)
+                .to(to)
+                .withSubject(subject)
+                .withPlainText(text)
+                .withHTMLText(html)
+                .withAttachments(attachments.map { x -> AttachmentResource(x.name, x.file) })
+                .buildEmail()
+            Runtime.mailer.sendMail(email, async)
+        } catch (e: Exception) {
+            Runtime.logger.error(e) { "Error sending email." }
+        }
     }
 }
