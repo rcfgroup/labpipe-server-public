07169c77f021604dd6ffc70dab2b38c0fa7a1178
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-07-03 16:26:21 +0100

    Update RecordService.kt

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/services/RecordService.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/services/RecordService.kt
@@ -1,46 +1,57 @@
 package uk.ac.le.ember.labpipe.server.services
 
-import uk.ac.le.ember.labpipe.server.auths.AuthManager
 import com.google.gson.Gson
-import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import io.javalin.core.security.SecurityUtil
 import org.bson.Document
+import org.bson.types.ObjectId
+import uk.ac.le.ember.labpipe.server.auths.AuthManager
 import uk.ac.le.ember.labpipe.server.notification.NotificationUtil
-import uk.ac.le.ember.labpipe.server.sessions.RuntimeData
-import uk.ac.le.ember.labpipe.server.sessions.StaticValue
+import uk.ac.le.ember.labpipe.server.sessions.Runtime
+import uk.ac.le.ember.labpipe.server.sessions.Statics
+import java.time.LocalDateTime
 
 object RecordService {
 
     // TODO Add support for multiple records batch upload
 
-    private fun saveRecord(jsonObject: JsonObject): String {
+    private fun saveRecord(jsonObject: JsonObject): String? {
         val formCode = jsonObject.get("form_code").asString
         val record = Document.parse(Gson().toJson(jsonObject))
         try {
-            val collection = RuntimeData.mongoDatabase
-                .getCollection("${StaticValue.DB_MONGO_COL_FORM_DATA_PREFIX}$formCode")
+            val collection = Runtime.mongoDatabase
+                .getCollection("${Statics.DB_MONGO_COL_FORM_DATA_PREFIX}$formCode")
             collection.insertOne(record)
         } catch (e: Exception) {
-            return "[Form: $formCode] data cannot be saved."
+            Runtime.logger.error(e) { "[Form: $formCode] data cannot be saved." }
+            return null
         }
-
-        return "[Form: $formCode] data is saved."
+        Runtime.logger.info { "[Form: $formCode] data is saved." }
+        val id = record.get("_id") as ObjectId
+        return id.toString()
     }
 
     fun routes() {
         println("Add record service routes.")
-        RuntimeData.labPipeServer.post("/api/record/add", { ctx ->
+        Runtime.server.post(Statics.API_PATH_RECORD_ADD, { ctx ->
             val operator = AuthManager.getUser(ctx)
             operator?.run {
                 val jsonParser = JsonParser()
                 val record = jsonParser.parse(ctx.body()).asJsonObject
                 val formCode = record.get("form_code").asString
+                record.addProperty("uploaded_by", operator.username)
+                record.addProperty("created", LocalDateTime.now().toString())
                 println(ctx.body())
-                saveRecord(record)
-                NotificationUtil.sendNotificationEmail(operator, formCode)
-                ctx.status(200)
+                val recordId = saveRecord(record)
+                if (recordId != null) {
+                    Runtime.logger.info { "Record saved [$recordId]" }
+                    NotificationUtil.sendNotificationEmail(operator, formCode)
+                    ctx.status(200)
+                } else {
+                    ctx.result("Record cannot be saved. Please retry or contact service manager.")
+                    ctx.status(500)
+                }
             }
         }, SecurityUtil.roles(AuthManager.ApiRole.AUTHORISED))
     }
