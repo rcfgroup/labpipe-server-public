7cdfd5aaae57e4e5ac9f84100b9c7362396c97eb
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-07-03 16:26:30 +0100

    Update DevService.kt

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/services/DevService.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/services/DevService.kt
@@ -1,26 +1,28 @@
 package uk.ac.le.ember.labpipe.server.services
 
-import uk.ac.le.ember.labpipe.server.auths.AuthManager
-import uk.ac.le.ember.labpipe.server.data.FormTemplate
 import io.javalin.core.security.SecurityUtil.roles
 import kotlinx.html.*
 import kotlinx.html.stream.appendHTML
-import org.litote.kmongo.*
-import uk.ac.le.ember.labpipe.server.sessions.RuntimeData
+import org.litote.kmongo.getCollection
+import uk.ac.le.ember.labpipe.server.auths.AuthManager
+import uk.ac.le.ember.labpipe.server.data.FormTemplate
+import uk.ac.le.ember.labpipe.server.sessions.Runtime
 
 object DevService {
     fun routes() {
         println("Loading developer service routes.")
-        RuntimeData.labPipeServer.get("/api/dev/html", { ctx -> ctx.html(testReplace()) },
+        Runtime.server.get(
+            "/api/dev/html", { ctx -> ctx.html(testReplace()) },
             roles(AuthManager.ApiRole.PUBLIC, AuthManager.ApiRole.AUTHORISED)
         )
-        RuntimeData.labPipeServer.get("/api/dev/form-template", { ctx -> ctx.json(testFormTemplate()) },
+        Runtime.server.get(
+            "/api/dev/form-template", { ctx -> ctx.json(testFormTemplate()) },
             roles(AuthManager.ApiRole.PUBLIC, AuthManager.ApiRole.AUTHORISED)
         )
     }
 
     fun testFormTemplate(): FormTemplate {
-        val col = RuntimeData.mongoDatabase.getCollection<FormTemplate>("FORM_TEMPLATES")
+        val col = Runtime.mongoDatabase.getCollection<FormTemplate>("FORM_TEMPLATES")
         return col.find().first() ?: FormTemplate("no_found", "Template Not Found")
     }
 
@@ -35,8 +37,10 @@ object DevService {
             appendln("<!DOCTYPE html>")
             appendHTML().html {
                 head {
-                    appendln("\n" +
-                            "<link rel=\"stylesheet\" href=\"https://unpkg.com/@clr/ui/clr-ui.min.css\" />")
+                    appendln(
+                        "\n" +
+                                "<link rel=\"stylesheet\" href=\"https://unpkg.com/@clr/ui/clr-ui.min.css\" />"
+                    )
                 }
                 body {
                     h1 { +"{{title}}" }
