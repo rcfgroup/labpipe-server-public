b6c3a2a26975fc42d2c84ef5f3aff0799812802e
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-07-03 16:26:26 +0100

    Update GeneralService.kt

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/services/GeneralService.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/services/GeneralService.kt
@@ -1,14 +1,27 @@
 package uk.ac.le.ember.labpipe.server.services
 
-import uk.ac.le.ember.labpipe.server.auths.AuthManager
 import io.javalin.core.security.SecurityUtil.roles
-import uk.ac.le.ember.labpipe.server.sessions.RuntimeData
+import uk.ac.le.ember.labpipe.server.auths.AuthManager
+import uk.ac.le.ember.labpipe.server.sessions.Runtime
+import uk.ac.le.ember.labpipe.server.sessions.Statics
 
 object GeneralService {
     fun routes() {
         println("Add general service routes.")
-        RuntimeData.labPipeServer.get("/api/general/connect", { ctx -> ctx.result("API server connected successfully.") }, roles(AuthManager.ApiRole.PUBLIC))
-        RuntimeData.labPipeServer.get("/api/general/connect-with-auth", { ctx -> ctx.result("API server connected successfully.") }, roles(AuthManager.ApiRole.AUTHORISED))
-        RuntimeData.labPipeServer.get("/api/general/connect-with-token", { ctx -> ctx.result("API server connected successfully.") }, roles(AuthManager.ApiRole.TOKEN_AUTHORISED))
+        Runtime.server.get(
+            Statics.API_PATH_GENERAL_CONNECT_PUBLIC,
+            { ctx -> ctx.result("API server connected successfully.") },
+            roles(AuthManager.ApiRole.PUBLIC)
+        )
+        Runtime.server.get(
+            Statics.API_PATH_GENERAL_CONNECT_AUTH,
+            { ctx -> ctx.result("API server connected successfully with authentication.") },
+            roles(AuthManager.ApiRole.AUTHORISED)
+        )
+        Runtime.server.get(
+            Statics.API_PATH_GENERAL_CONNECT_TOKEN,
+            { ctx -> ctx.result("API server connected successfully with token.") },
+            roles(AuthManager.ApiRole.TOKEN_AUTHORISED)
+        )
     }
 }
