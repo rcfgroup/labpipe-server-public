c8720048c122fb9518538470ddea2bd0d66a6bae
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-07-04 16:23:54 +0100

    Update App.kt

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,10 +1,14 @@
-import uk.ac.le.ember.labpipe.server.cmdline.LPServerCmdLine
+import com.github.ajalt.clikt.core.subcommands
+import uk.ac.le.ember.labpipe.server.cmdline.*
 
 
 fun main(args: Array<String>) {
-    if (args.isEmpty()) {
-        LPServerCmdLine().main(arrayOf("--help"))
-    } else {
-        LPServerCmdLine().main(args)
+    var argArray = args
+    if (argArray.isEmpty()) {
+        argArray = arrayOf("--help")
     }
+    LPServerCmdLine().subcommands(
+        Config().subcommands(Server(), Database(), Email()),
+        Check(), Run(), Init()
+    ).main(argArray)
 }
