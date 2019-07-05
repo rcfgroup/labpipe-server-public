e7e89a99f69b9d46a91667c46b6117dff5da3a83
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-07-05 16:34:10 +0100

    Update LPServerCmdLine.kt
    Started working on init function

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/cmdline/LPServerCmdLine.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/cmdline/LPServerCmdLine.kt
@@ -6,19 +6,25 @@ import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.types.int
+import io.javalin.Javalin
 import org.apache.commons.configuration2.PropertiesConfiguration
 import org.apache.commons.configuration2.builder.fluent.Configurations
 import org.apache.commons.configuration2.ex.ConfigurationException
 import uk.ac.le.ember.labpipe.server.auths.AuthManager
+import uk.ac.le.ember.labpipe.server.data.AccessToken
 import uk.ac.le.ember.labpipe.server.data.LPConfig
 import uk.ac.le.ember.labpipe.server.db.DatabaseUtil
 import uk.ac.le.ember.labpipe.server.notification.EmailUtil
 import uk.ac.le.ember.labpipe.server.services.*
+import uk.ac.le.ember.labpipe.server.sessions.PropertyFields
 import uk.ac.le.ember.labpipe.server.sessions.RequiredMongoDBCollections
 import uk.ac.le.ember.labpipe.server.sessions.Runtime
 import uk.ac.le.ember.labpipe.server.sessions.Statics
 import java.io.File
 import java.nio.file.Paths
+import org.litote.kmongo.*
+import org.mindrot.jbcrypt.BCrypt
+import uk.ac.le.ember.labpipe.server.data.ClientSettings
 
 
 fun updateConfig(key: String, value: String?) {
@@ -71,18 +77,18 @@ fun readConfig(): PropertiesConfiguration? {
     val configFile = File(Statics.DEFAULT_CONFIG_FILE_NAME)
     val configs = Configurations()
     if (!configFile.exists()) {
-        updateConfig(key = Statics.PROPS_FIELD_SERVER_PORT, value = 4567)
-        updateConfig(key = Statics.PROPS_FIELD_DB_HOST, value = "localhost")
-        updateConfig(key = Statics.PROPS_FIELD_DB_PORT, value = 27017)
-        updateConfig(key = Statics.PROPS_FIELD_DB_NAME, value = "labpipe-dev")
+        updateConfig(key = PropertyFields.SERVER_PORT.value, value = 4567)
+        updateConfig(key = PropertyFields.DB_HOST.value, value = "localhost")
+        updateConfig(key = PropertyFields.DB_PORT.value, value = 27017)
+        updateConfig(key = PropertyFields.DB_NAME.value, value = "labpipe-dev")
 
-        updateConfig(key = Statics.PROPS_FIELD_EMAIL_HOST, value = "localhost")
-        updateConfig(key = Statics.PROPS_FIELD_EMAIL_PORT, value = 25)
-        updateConfig(key = Statics.PROPS_FIELD_EMAIL_NOTIFICATION_NAME, value = "LabPipe Notification")
-        updateConfig(key = Statics.PROPS_FIELD_EMAIL_NOTIFICATION_ADDR, value = "no-reply@labpipe.org")
+        updateConfig(key = PropertyFields.EMAIL_HOST.value, value = "localhost")
+        updateConfig(key = PropertyFields.EMAIL_PORT.value, value = 25)
+        updateConfig(key = PropertyFields.EMAIL_NOTIFIER_NAME.value, value = "LabPipe Notification")
+        updateConfig(key = PropertyFields.EMAIL_NOTIFIER_ADDR.value, value = "no-reply@labpipe.org")
 
         val defaultCacheDir = Paths.get(System.getProperty("user.home"), "labpipe").toString()
-        updateConfig(key = Statics.PROPS_FIELD_PATH_CACHE, value = defaultCacheDir)
+        updateConfig(key = PropertyFields.PATH_CACHE.value, value = defaultCacheDir)
 
         echo("Using config file: [${configFile.absolutePath}]")
         echo("Default settings:")
@@ -111,63 +117,50 @@ fun importConfig() {
     val properties = readConfig()
     properties?.run {
         Runtime.config = LPConfig(
-            when {
-                properties.containsKey(Statics.PROPS_FIELD_SERVER_PORT) -> properties.getInt(Statics.PROPS_FIELD_SERVER_PORT)
-                else -> 4567
-            }
+            serverPort = if (properties.containsKey(PropertyFields.SERVER_PORT.value)) properties.getInt(PropertyFields.SERVER_PORT.value)
+            else 4567
         )
-        Runtime.config.tempPath = when {
-            properties.containsKey(Statics.PROPS_FIELD_PATH_CACHE) -> properties.getString(Statics.PROPS_FIELD_PATH_CACHE)
-            else -> Paths.get(System.getProperty("user.home"), "labpipe").toString()
-        }
-        Runtime.config.dbHost = when {
-            properties.containsKey(Statics.PROPS_FIELD_DB_HOST) -> properties.getString(Statics.PROPS_FIELD_DB_HOST)
-            else -> "localhost"
-        }
-        Runtime.config.dbPort = when {
-            properties.containsKey(Statics.PROPS_FIELD_DB_PORT) -> properties.getInt(Statics.PROPS_FIELD_DB_PORT)
-            else -> 27017
-        }
-        Runtime.config.dbName = when {
-            properties.containsKey(Statics.PROPS_FIELD_DB_NAME) -> properties.getString(Statics.PROPS_FIELD_DB_NAME)
-            else -> "labpipe-dev"
-        }
-        Runtime.config.dbUser = when {
-            properties.containsKey(Statics.PROPS_FIELD_DB_USER) -> properties.getString(Statics.PROPS_FIELD_DB_USER)
-            else -> null
-        }
-        Runtime.config.dbPass = when {
-            properties.containsKey(Statics.PROPS_FIELD_DB_PASS) -> properties.getString(Statics.PROPS_FIELD_DB_PASS)
-            else -> null
-        }
-        Runtime.config.emailHost = when {
-            properties.containsKey(Statics.PROPS_FIELD_EMAIL_HOST) -> properties.getString(Statics.PROPS_FIELD_EMAIL_HOST)
-            else -> "localhost"
-        }
-        Runtime.config.emailPort = when {
-            properties.containsKey(Statics.PROPS_FIELD_EMAIL_PORT) -> properties.getInt(Statics.PROPS_FIELD_EMAIL_PORT)
-            else -> 25
-        }
-        Runtime.config.emailUser = when {
-            properties.containsKey(Statics.PROPS_FIELD_EMAIL_USER) -> properties.getString(Statics.PROPS_FIELD_EMAIL_USER)
-            else -> null
-        }
-        Runtime.config.emailPass = when {
-            properties.containsKey(Statics.PROPS_FIELD_EMAIL_PASS) -> properties.getString(Statics.PROPS_FIELD_EMAIL_PASS)
-            else -> null
-        }
-        Runtime.config.notificationEmailName = when {
-            properties.containsKey(Statics.PROPS_FIELD_EMAIL_NOTIFICATION_NAME) -> properties.getString(Statics.PROPS_FIELD_EMAIL_NOTIFICATION_NAME)
-            else -> "LabPipe Notification"
-        }
-        Runtime.config.notificationEmailAddress = when {
-            properties.containsKey(Statics.PROPS_FIELD_EMAIL_NOTIFICATION_ADDR) -> properties.getString(Statics.PROPS_FIELD_EMAIL_NOTIFICATION_ADDR)
-            else -> "no-reply@labpipe.org"
-        }
+        Runtime.config.tempPath =
+            if (properties.containsKey(PropertyFields.PATH_CACHE.value)) properties.getString(PropertyFields.PATH_CACHE.value)
+            else Paths.get(System.getProperty("user.home"), "labpipe").toString()
+        Runtime.config.dbHost =
+            if (properties.containsKey(PropertyFields.DB_HOST.value)) properties.getString(PropertyFields.DB_HOST.value)
+            else "localhost"
+        Runtime.config.dbPort =
+            if (properties.containsKey(PropertyFields.DB_PORT.value)) properties.getInt(PropertyFields.DB_PORT.value)
+            else 27017
+        Runtime.config.dbName =
+            if (properties.containsKey(PropertyFields.DB_NAME.value)) properties.getString(PropertyFields.DB_NAME.value)
+            else "labpipe"
+        Runtime.config.dbUser =
+            if (properties.containsKey(PropertyFields.DB_USER.value)) properties.getString(PropertyFields.DB_USER.value)
+            else null
+        Runtime.config.dbPass =
+            if (properties.containsKey(PropertyFields.DB_PASS.value)) properties.getString(PropertyFields.DB_PASS.value)
+            else null
+        Runtime.config.emailHost =
+            if (properties.containsKey(PropertyFields.EMAIL_HOST.value)) properties.getString(PropertyFields.EMAIL_HOST.value)
+            else "localhost"
+        Runtime.config.emailPort =
+            if (properties.containsKey(PropertyFields.EMAIL_PORT.value)) properties.getInt(PropertyFields.EMAIL_PORT.value)
+            else 25
+        Runtime.config.emailUser =
+            if (properties.containsKey(PropertyFields.EMAIL_USER.value)) properties.getString(PropertyFields.EMAIL_USER.value)
+            else null
+        Runtime.config.emailPass =
+            if (properties.containsKey(PropertyFields.EMAIL_PASS.value)) properties.getString(PropertyFields.EMAIL_PASS.value)
+            else null
+        Runtime.config.notificationEmailName =
+            if (properties.containsKey(PropertyFields.EMAIL_NOTIFIER_NAME.value)) properties.getString(PropertyFields.EMAIL_NOTIFIER_NAME.value)
+            else "LabPipe Notification"
+        Runtime.config.notificationEmailAddress =
+            if (properties.containsKey(PropertyFields.EMAIL_NOTIFIER_ADDR.value)) properties.getString(PropertyFields.EMAIL_NOTIFIER_ADDR.value)
+            else "no-reply@labpipe.org"
     }
 }
 
 fun startServer() {
+    Runtime.server = Javalin.create()
     AuthManager.setManager()
     GeneralService.routes()
     ParameterService.routes()
@@ -197,8 +190,8 @@ class Server : CliktCommand(name = "server", help = "Configure server") {
     private val cache by option("--cache", help = "cache directory")
 
     override fun run() {
-        updateConfig(key = Statics.PROPS_FIELD_SERVER_PORT, value = port)
-        updateConfig(key = Statics.PROPS_FIELD_PATH_CACHE, value = cache)
+        updateConfig(key = PropertyFields.SERVER_PORT.value, value = port)
+        updateConfig(key = PropertyFields.PATH_CACHE.value, value = cache)
     }
 }
 
@@ -210,11 +203,11 @@ class Database : CliktCommand(name = "db", help = "Configure database server") {
     private val pswd by option("--pass", help = "database password")
 
     override fun run() {
-        updateConfig(key = Statics.PROPS_FIELD_DB_HOST, value = host)
-        updateConfig(key = Statics.PROPS_FIELD_DB_PORT, value = port)
-        updateConfig(key = Statics.PROPS_FIELD_DB_NAME, value = name)
-        updateConfig(key = Statics.PROPS_FIELD_DB_USER, value = user)
-        updateConfig(key = Statics.PROPS_FIELD_DB_PASS, value = pswd)
+        updateConfig(key = PropertyFields.DB_HOST.value, value = host)
+        updateConfig(key = PropertyFields.DB_PORT.value, value = port)
+        updateConfig(key = PropertyFields.DB_NAME.value, value = name)
+        updateConfig(key = PropertyFields.DB_USER.value, value = user)
+        updateConfig(key = PropertyFields.DB_PASS.value, value = pswd)
     }
 }
 
@@ -230,16 +223,16 @@ class Email : CliktCommand(name = "email", help = "Configure email server") {
     )
 
     override fun run() {
-        updateConfig(key = Statics.PROPS_FIELD_EMAIL_HOST, value = host)
-        updateConfig(key = Statics.PROPS_FIELD_EMAIL_PORT, value = port)
-        updateConfig(key = Statics.PROPS_FIELD_EMAIL_USER, value = user)
-        updateConfig(key = Statics.PROPS_FIELD_EMAIL_PASS, value = pswd)
+        updateConfig(key = PropertyFields.EMAIL_HOST.value, value = host)
+        updateConfig(key = PropertyFields.EMAIL_PORT.value, value = port)
+        updateConfig(key = PropertyFields.EMAIL_USER.value, value = user)
+        updateConfig(key = PropertyFields.EMAIL_PASS.value, value = pswd)
         updateConfig(
-            key = Statics.PROPS_FIELD_EMAIL_NOTIFICATION_NAME,
+            key = PropertyFields.EMAIL_NOTIFIER_NAME.value,
             value = notifierName
         )
         updateConfig(
-            key = Statics.PROPS_FIELD_EMAIL_NOTIFICATION_ADDR,
+            key = PropertyFields.EMAIL_NOTIFIER_ADDR.value,
             value = notifierAddr
         )
     }
@@ -265,20 +258,33 @@ class Run : CliktCommand(name = "run", help = "Run server") {
         EmailUtil.connect()
         EmailUtil.testConnection()
         DatabaseUtil.testConnection()
+        startServer()
     }
 }
 
 class Init : CliktCommand(name = "init", help = "Init server") {
 
     override fun run() {
-        importConfig()
-        DatabaseUtil.connect()
-        EmailUtil.connect()
-        EmailUtil.testConnection()
-        DatabaseUtil.testConnection()
-        RequiredMongoDBCollections.values().forEach { Runtime.mongoDatabase.createCollection(it.value)}
-        val cols = RequiredMongoDBCollections.values().map { it.value }.toMutableList()
-        cols.removeAll(Runtime.mongoDatabase.listCollectionNames())
-        cols.forEach{ Runtime.mongoDatabase.createCollection(it)}
+        echo("The init function will be available in next release.")
+//        importConfig()
+//        DatabaseUtil.connect()
+//        EmailUtil.connect()
+//        EmailUtil.testConnection()
+//        DatabaseUtil.testConnection()
+//        val cols = RequiredMongoDBCollections.values().map { it.value }.toMutableList()
+//        cols.removeAll(Runtime.mongoDatabase.listCollectionNames())
+//        cols.forEach { Runtime.mongoDatabase.createCollection(it) }
+//        Runtime.mongoDatabase.getCollection<AccessToken>(RequiredMongoDBCollections.ACCESS_TOKENS.value)
+//            .insertOne(AccessToken(token = "token", keyHash = BCrypt.hashpw("key", BCrypt.gensalt())))
+//        val clientSettings = ClientSettings(code = "client_init", name = "Parameter list for client init")
+//        clientSettings.value = mutableListOf(
+//            RequiredMongoDBCollections.LOCATIONS.value,
+//            RequiredMongoDBCollections.OPERATORS.value,
+//            RequiredMongoDBCollections.STUDIES.value,
+//            RequiredMongoDBCollections.INSTRUMENTS.value,
+//            RequiredMongoDBCollections.COLLECTORS.value
+//        )
+//        Runtime.mongoDatabase.getCollection<ClientSettings>(RequiredMongoDBCollections.CLIENT_SETTINGS.value)
+//            .insertOne(clientSettings)
     }
 }
