65f844186d9741835185a4b0b89194d35eb4259d
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-07-05 16:34:35 +0100

    Update ParameterService.kt

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/services/ParameterService.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/services/ParameterService.kt
@@ -5,6 +5,7 @@ import org.litote.kmongo.aggregate
 import org.litote.kmongo.excludeId
 import org.litote.kmongo.project
 import uk.ac.le.ember.labpipe.server.auths.AuthManager
+import uk.ac.le.ember.labpipe.server.sessions.ApiPath
 import uk.ac.le.ember.labpipe.server.sessions.Runtime
 import uk.ac.le.ember.labpipe.server.sessions.Statics
 
@@ -13,15 +14,13 @@ object ParameterService {
     private fun getWithParameterName(paramName: String): List<Any> {
         println("Getting parameter [$paramName]")
         val col = Runtime.mongoDatabase.getCollection(paramName)
-        return listOf(
-            col.aggregate<Any>(project(excludeId()))
-        )
+        return col.aggregate<Any>(project(excludeId())).toMutableList()
     }
 
     fun routes() {
         println("Add parameter service routes.")
         Runtime.server.get(
-            Statics.API_PATH_PARAM_WITH_NAME, { ctx -> ctx.json(getWithParameterName(ctx.pathParam("paramName"))) },
+            ApiPath.PARAM_WITH_NAME.value, { ctx -> ctx.json(getWithParameterName(ctx.pathParam("paramName"))) },
             roles(AuthManager.ApiRole.AUTHORISED, AuthManager.ApiRole.TOKEN_AUTHORISED)
         )
     }
