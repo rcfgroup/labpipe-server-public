6a40abb0da5bc6bfc5c183e0c73fdc20be48f2dc
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-07-05 16:35:06 +0100

    Update Statics.kt

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/sessions/Statics.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/sessions/Statics.kt
@@ -2,22 +2,6 @@ package uk.ac.le.ember.labpipe.server.sessions
 
 class Statics {
     companion object {
-        const val PROPS_FIELD_SERVER_PORT = "server.port"
-
-        const val PROPS_FIELD_DB_HOST = "database.host"
-        const val PROPS_FIELD_DB_PORT = "database.port"
-        const val PROPS_FIELD_DB_NAME = "database.name"
-        const val PROPS_FIELD_DB_USER = "database.user"
-        const val PROPS_FIELD_DB_PASS = "database.pass"
-
-        const val PROPS_FIELD_EMAIL_HOST = "mail.host"
-        const val PROPS_FIELD_EMAIL_PORT = "mail.port"
-        const val PROPS_FIELD_EMAIL_USER = "mail.user"
-        const val PROPS_FIELD_EMAIL_PASS = "mail.pass"
-        const val PROPS_FIELD_EMAIL_NOTIFICATION_NAME = "mail.notifier.name"
-        const val PROPS_FIELD_EMAIL_NOTIFICATION_ADDR = "mail.notifier.addr"
-
-        const val PROPS_FIELD_PATH_CACHE = "path.cache"
 
         const val DEFAULT_CONFIG_FILE_NAME = "config.ini"
 
@@ -25,29 +9,53 @@ class Statics {
         const val DB_COL_FORM_DATA_PREFIX = "FORM_DATA_"
 
 
-        const val API_PATH_ROOT = "/api"
-        const val API_PATH_FORM_TEMPLATE = "$API_PATH_ROOT/form/template/:formCode"
-        const val API_PATH_GENERAL_SERVICE = "$API_PATH_ROOT/general"
-        const val API_PATH_GENERAL_CONNECT_PUBLIC = "$API_PATH_GENERAL_SERVICE/connect"
-        const val API_PATH_GENERAL_CONNECT_AUTH = "$API_PATH_GENERAL_SERVICE/connect-with-auth"
-        const val API_PATH_GENERAL_CONNECT_TOKEN = "$API_PATH_GENERAL_SERVICE/connect-with-token"
-        const val API_PATH_RECORD_SERVICE = "$API_PATH_ROOT/record"
-        const val API_PATH_RECORD_ADD = "$API_PATH_RECORD_SERVICE/add"
-        const val API_PATH_PARAM_SERVICE = "$API_PATH_ROOT/parameter"
-        const val API_PATH_PARAM_WITH_NAME = "$API_PATH_PARAM_SERVICE/name/:paramName"
+        
     }
 }
 
+enum class ApiPath(val value:String) {
+    ROOT("/api"),
+    FORM_TEMPLATE_WITH_FORMCODE("${ROOT.value}/form/template/code/:formCode"),
+    FORM_TEMPLATE_WITH_STUDYCODE_INSTRUMENTCODE("${ROOT.value}/form/template/study/:studyCode/instrument/:instrumentCode"),
+    GENERAL_SERVICE("${ROOT.value}/general"),
+    GENERAL_CONNECT_PUBLIC("${GENERAL_SERVICE.value}/connect"),
+    GENERAL_CONNECT_AUTH("${GENERAL_SERVICE.value}/connect-with-auth"),
+    GENERAL_CONNECT_TOKEN("${GENERAL_SERVICE.value}/connect-with-token"),
+    RECORD_SERVICE("${ROOT.value}/record"),
+    RECORD_ADD("${RECORD_SERVICE.value}/add"),
+    PARAM_SERVICE("${ROOT.value}/parameter"),
+    PARAM_WITH_NAME("${PARAM_SERVICE.value}/name/:paramName")
+}
+
+enum class PropertyFields(val value: String) {
+    SERVER_PORT("server.port"),
+    DB_HOST("database.host"),
+    DB_PORT("database.port"),
+    DB_NAME("database.name"),
+    DB_USER("database.user"),
+    DB_PASS("database.pass"),
+
+    EMAIL_HOST("mail.host"),
+    EMAIL_PORT("mail.port"),
+    EMAIL_USER("mail.user"),
+    EMAIL_PASS("mail.pass"),
+    EMAIL_NOTIFIER_NAME("mail.notifier.name"),
+    EMAIL_NOTIFIER_ADDR("mail.notifier.addr"),
+    PATH_CACHE("path.cache"),
+}
+
 enum class RequiredMongoDBCollections(val value: String) {
-    ACCESS_TOKEN("ACCESS_TOKENS"),
-    ROLE("ROLES"),
-    CLIENT_SETTING("CLIENT_SETTINGS"),
+    ACCESS_TOKENS("ACCESS_TOKENS"),
+    ROLES("ROLES"),
+    CLIENT_SETTINGS("CLIENT_SETTINGS"),
     API_ACCESS_ROLES("API_ACCESS_ROLES"),
     OPERATORS("OPERATORS"),
     STUDIES("STUDIES"),
     FORM_TEMPLATES("FORM_TEMPLATES"),
     REPORT_TEMPLATES("REPORT_TEMPLATES"),
     INSTRUMENTS("INSTRUMENTS"),
+    COLLECTORS("COLLECTORS"),
+    SAMPLE_TYPES("SAMPLE_TYPES"),
     LOCATIONS("LOCATIONS"),
     EMAIL_GROUPS("EMAIL_GROUPS")
 }
