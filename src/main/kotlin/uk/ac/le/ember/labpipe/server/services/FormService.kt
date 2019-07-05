62e8eb949664531756118e735519e9708c71671b
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-07-05 16:34:29 +0100

    Update FormService.kt

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/services/FormService.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/services/FormService.kt
@@ -1,11 +1,10 @@
 package uk.ac.le.ember.labpipe.server.services
 
 import io.javalin.core.security.SecurityUtil
-import org.litote.kmongo.eq
-import org.litote.kmongo.findOne
-import org.litote.kmongo.getCollection
+import org.litote.kmongo.*
 import uk.ac.le.ember.labpipe.server.auths.AuthManager
 import uk.ac.le.ember.labpipe.server.data.FormTemplate
+import uk.ac.le.ember.labpipe.server.sessions.ApiPath
 import uk.ac.le.ember.labpipe.server.sessions.RequiredMongoDBCollections
 import uk.ac.le.ember.labpipe.server.sessions.Runtime
 import uk.ac.le.ember.labpipe.server.sessions.Statics
@@ -14,7 +13,11 @@ object FormService {
     fun routes() {
         println("Add form service routes")
         Runtime.server.get(
-            Statics.API_PATH_FORM_TEMPLATE, { ctx -> getFormTemplate(ctx.pathParam("formCode"))?.let { ctx.json(it) } },
+            ApiPath.FORM_TEMPLATE_WITH_FORMCODE.value, { ctx -> getFormTemplate(ctx.pathParam("formCode"))?.let { ctx.json(it) } },
+            SecurityUtil.roles(AuthManager.ApiRole.PUBLIC, AuthManager.ApiRole.AUTHORISED)
+        )
+        Runtime.server.get(
+            ApiPath.FORM_TEMPLATE_WITH_STUDYCODE_INSTRUMENTCODE.value, { ctx -> ctx.json(getFormTemplate(ctx.pathParam("studyCode"), ctx.pathParam("instrumentCode"))) },
             SecurityUtil.roles(AuthManager.ApiRole.PUBLIC, AuthManager.ApiRole.AUTHORISED)
         )
     }
@@ -23,5 +26,11 @@ object FormService {
     fun getFormTemplate(formCode: String): FormTemplate? {
         val col = Runtime.mongoDatabase.getCollection<FormTemplate>(RequiredMongoDBCollections.FORM_TEMPLATES.value)
         return col.findOne(FormTemplate::code eq formCode)
+    }
+
+
+    fun getFormTemplate(studyCode: String, instrumentCode: String): List<FormTemplate> {
+        val col = Runtime.mongoDatabase.getCollection<FormTemplate>(RequiredMongoDBCollections.FORM_TEMPLATES.value)
+        return col.find(FormTemplate::studyCode eq studyCode, FormTemplate::instrumentCode eq instrumentCode).toMutableList()
     }
 }
