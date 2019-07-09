d74d93640231d21b1b62c3249b7e964d87aa53f5
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-07-09 16:55:11 +0100

    Update DevService.kt

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/services/DevService.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/services/DevService.kt
@@ -1,8 +1,8 @@
 package uk.ac.le.ember.labpipe.server.services
 
+import com.google.gson.JsonObject
 import io.javalin.core.security.SecurityUtil.roles
-import kotlinx.html.*
-import kotlinx.html.stream.appendHTML
+import j2html.TagCreator.*
 import org.litote.kmongo.getCollection
 import uk.ac.le.ember.labpipe.server.auths.AuthManager
 import uk.ac.le.ember.labpipe.server.data.FormTemplate
@@ -12,42 +12,8 @@ object DevService {
     fun routes() {
         println("Loading developer service routes.")
         Runtime.server.get(
-            "/api/dev/html", { ctx -> ctx.html(testReplace()) },
+            "/api/dev/form-template", { ctx -> ctx.json({}) },
             roles(AuthManager.ApiRole.PUBLIC, AuthManager.ApiRole.AUTHORISED)
         )
-        Runtime.server.get(
-            "/api/dev/form-template", { ctx -> ctx.json(testFormTemplate()) },
-            roles(AuthManager.ApiRole.PUBLIC, AuthManager.ApiRole.AUTHORISED)
-        )
-    }
-
-    fun testFormTemplate(): FormTemplate {
-        val col = Runtime.mongoDatabase.getCollection<FormTemplate>("FORM_TEMPLATES")
-        return col.find().first() ?: FormTemplate("no_found", "Template Not Found")
-    }
-
-    fun testReplace(): String {
-        var html = testHtml()
-        html = html.replace("{{title}}", "Report title")
-        return html
-    }
-
-    fun testHtml(): String {
-        return buildString {
-            appendln("<!DOCTYPE html>")
-            appendHTML().html {
-                head {
-                    appendln(
-                        "\n" +
-                                "<link rel=\"stylesheet\" href=\"https://unpkg.com/@clr/ui/clr-ui.min.css\" />"
-                    )
-                }
-                body {
-                    h1 { +"{{title}}" }
-                    a("http://kotlinlang.org") { +"link" }
-                }
-            }
-            appendln()
-        }
     }
 }
