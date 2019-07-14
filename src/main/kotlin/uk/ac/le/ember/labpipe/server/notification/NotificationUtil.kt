df5258617e1300e17386e640456e86df1306b1e6
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-07-15 00:35:32 +0100

    Update NotificationUtil.kt

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/notification/NotificationUtil.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/notification/NotificationUtil.kt
@@ -1,8 +1,6 @@
 package uk.ac.le.ember.labpipe.server.notification
 
-import com.google.gson.JsonArray
 import com.google.gson.JsonObject
-import j2html.TagCreator.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.litote.kmongo.*
@@ -10,21 +8,23 @@ import org.simplejavamail.email.Recipient
 import uk.ac.le.ember.labpipe.server.data.EmailGroup
 import uk.ac.le.ember.labpipe.server.data.FormTemplate
 import uk.ac.le.ember.labpipe.server.data.Operator
-import uk.ac.le.ember.labpipe.server.data.ReportTemplate
-import uk.ac.le.ember.labpipe.server.sessions.*
+import uk.ac.le.ember.labpipe.server.sessions.NotificationStyle
+import uk.ac.le.ember.labpipe.server.sessions.RequiredMongoDBCollections
+import uk.ac.le.ember.labpipe.server.sessions.Runtime
 
 object NotificationUtil {
     fun sendNotificationEmail(operator: Operator, formCode: String, record: JsonObject) {
         GlobalScope.launch {
-            val recordForm = Runtime.mongoDatabase.getCollection<FormTemplate>(RequiredMongoDBCollections.FORM_TEMPLATES.value)
-                .findOne { FormTemplate::code eq formCode }
+            val recordForm =
+                Runtime.mongoDatabase.getCollection<FormTemplate>(RequiredMongoDBCollections.FORM_TEMPLATES.value)
+                    .findOne { FormTemplate::code eq formCode }
             recordForm?.run {
                 val recipients = getEmailRecipients(operator, recordForm)
                 recipients?.run {
                     for (r in recipients) {
                         println("Recipient: ${r.name} <${r.address}>")
                     }
-                    var htmlReport = generateReportHtml(operator, recordForm, record)
+                    var htmlReport = ReportUtil.generateHtml(operator, recordForm, record)
                     EmailUtil.sendEmail(
                         from = Recipient(
                             Runtime.config.notificationEmailName,
@@ -57,7 +57,8 @@ object NotificationUtil {
             else -> {
                 val emailGroups =
                     Runtime.mongoDatabase.getCollection<EmailGroup>(RequiredMongoDBCollections.EMAIL_GROUPS.value)
-                        .find(EmailGroup::code `in` operator.notificationGroup, EmailGroup::formCode eq form.code).toMutableList()
+                        .find(EmailGroup::code `in` operator.notificationGroup, EmailGroup::formCode eq form.code)
+                        .toMutableList()
                 println("Email Groups: $emailGroups")
                 val adminUsernames = emailGroups.map { g -> g.admin }.flatten()
                 println("Admins: $adminUsernames")
@@ -82,101 +83,5 @@ object NotificationUtil {
                 }
             }
         }
-    }
-
-    fun generateReportHtml(operator: Operator, form: FormTemplate, formData: JsonObject): String? {
-        val reportTemplate = Runtime.mongoDatabase.getCollection<ReportTemplate>(RequiredMongoDBCollections.REPORT_TEMPLATES.value)
-            .findOne (ReportTemplate::formCode eq form.code, ReportTemplate::active eq true )
-        reportTemplate?.run {
-            Runtime.logger.info { "Report template found." }
-            Runtime.logger.info { reportTemplate.htmlTemplate.size }
-            val elements = reportTemplate.htmlTemplate
-            elements.sortedWith(compareBy { it.order })
-            var reportBody = body()
-            var validValueRegex = Regex("(.*)::(.*)")
-            elements.forEach {
-                run {
-                    Runtime.logger.info { "Processing report element: $it" }
-                    when (it.type) {
-                        ReportElementType.REPORT_TITLE.value -> {
-                            val valueArray = it.value.split("::")
-                            val value: String = when (valueArray[0]) {
-                                ReportElementValueSource.STATIC_VALUE.value -> valueArray[1]
-                                ReportElementValueSource.FORM_DATA.value -> ReportUtil.getFormDataAsString(formData, valueArray[1])
-                                ReportElementValueSource.FORM_TEMPLATE.value -> form.getProperty(valueArray[1]) ?: "Unable to get ${valueArray[1]} from form template"
-                                else -> "Invalid format in report config"
-                            }
-                            println("H1: $value")
-                            reportBody.with(h1(value))
-                        }
-                        ReportElementType.SECTION_TITLE.value -> {
-                            val valueArray = it.value.split("::")
-                            val value: String = when (valueArray[0]) {
-                                ReportElementValueSource.STATIC_VALUE.value -> valueArray[1]
-                                ReportElementValueSource.FORM_DATA.value -> ReportUtil.getFormDataAsString(formData, valueArray[1])
-                                ReportElementValueSource.FORM_TEMPLATE.value -> form.getProperty(valueArray[1]) ?: "Unable to get valueArray[1] from form template"
-                                else -> "Invalid format in report config"
-                            }
-                            println("H3: $value")
-                            reportBody.with(h3(value))
-                        }
-                        ReportElementType.TABLE_KEYVALUE.value -> {
-                            val valueArray = it.value.split("::")
-                            val value: JsonObject? = when (valueArray[0]) {
-                                ReportElementValueSource.FORM_DATA.value -> formData.getAsJsonObject(valueArray[1])
-                                else -> null
-                            }
-                            var table = table().withClasses("table", "table-responsive")
-                            value?.run {
-                                val entries = value.entrySet()
-                                val chunkedEntries = entries.chunked(4)
-
-                                chunkedEntries.forEach { chunk ->
-                                    var row = tr()
-                                    chunk.forEach {
-                                        row.with(
-                                            th(it.key), td(it.value.asString)
-                                        )
-                                    }
-                                    table.with(row)
-                                }
-                            }
-                            reportBody.with(table)
-                        }
-                        ReportElementType.TABLE_LOOP.value -> {
-                            println("To be implemented")
-                            // TODO
-                        }
-                        ReportElementType.LIST.value -> {
-                            val valueArray = it.value.split("::")
-                            val value: JsonArray? = when (valueArray[0]) {
-                                ReportElementValueSource.FORM_DATA.value -> ReportUtil.getFormDataAsArray(formData, valueArray[1])
-                                else -> null
-                            }
-                            var list = ol()
-                            value?.run {
-                                value.forEach { list.with(li(it.asString)) }
-                            }
-                            reportBody.with(list)
-                        }
-                        else -> reportBody.with(p("Invalid format in report config"))
-                    }
-                }
-            }
-            val report = html(
-                head(
-                    link().withRel("stylesheet").withHref("https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css")
-                ),
-                reportBody
-            )
-            val result = report.renderFormatted()
-            println(result)
-            return result
-        }
-        return null
-    }
-
-    fun generateReportText(operator: Operator, form: FormTemplate, formData: JsonObject) {
-        // TODO
     }
 }
