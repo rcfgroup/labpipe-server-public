b8504c37f82e1772f14bbcb1af27f0b18ff7a5ec
Bo Zhao <3310824+colin-bz@users.noreply.github.com>
2019-09-23 00:50:05 +0100

    Update GeneralService.kt

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/services/GeneralService.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/services/GeneralService.kt
@@ -3,7 +3,6 @@ package uk.ac.le.ember.labpipe.server.services
 import io.javalin.core.security.SecurityUtil.roles
 import uk.ac.le.ember.labpipe.server.AuthManager
 import uk.ac.le.ember.labpipe.server.Constants
-import uk.ac.le.ember.labpipe.server.sessions.ApiPath
 import uk.ac.le.ember.labpipe.server.sessions.Runtime
 
 object GeneralService {
@@ -11,17 +10,17 @@ object GeneralService {
         println("Add general service routes.")
         Runtime.server.get(
             Constants.API.GENERAL.CONN_PUBLIC,
-            { ctx -> ctx.result(Constants.MSG.CONN_PUBLIC_SUCCESS) },
+            { ctx -> ctx.result(Constants.MESSAGES.CONN_PUBLIC_SUCCESS) },
             roles(AuthManager.ApiRole.PUBLIC)
         )
         Runtime.server.get(
             Constants.API.GENERAL.CONN_AUTH,
-            { ctx -> ctx.result(Constants.MSG.CONN_AUTH_SUCCESS) },
+            { ctx -> ctx.result(Constants.MESSAGES.CONN_AUTH_SUCCESS) },
             roles(AuthManager.ApiRole.AUTHORISED)
         )
         Runtime.server.get(
             Constants.API.GENERAL.CONN_TOKEN,
-            { ctx -> ctx.result(Constants.MSG.CONN_TOKEN_SUCCESS) },
+            { ctx -> ctx.result(Constants.MESSAGES.CONN_TOKEN_SUCCESS) },
             roles(AuthManager.ApiRole.TOKEN_AUTHORISED)
         )
     }
