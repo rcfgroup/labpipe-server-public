3c9b9142cd7b7be4851f4387c6660c339c003258
Bo Zhao <bo.zhao@bz.dev>
2020-11-02 02:42:42 +0000

    Multiple updates
    1. DatabaseUtil is now DatabaseController
    2. Move database init scripts into DatabaseController.init()

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/cmdline/CommandLineUtil.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/cmdline/CommandLineUtil.kt
@@ -1,39 +1,29 @@
 package uk.ac.le.ember.labpipe.server.cmdline
 
 import com.github.ajalt.clikt.core.CliktCommand
-import com.github.ajalt.clikt.output.TermUi.echo
-import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
-import com.github.ajalt.clikt.parameters.types.int
 import io.javalin.Javalin
-import kotlinx.coroutines.runBlocking
-import org.apache.commons.configuration2.PropertiesConfiguration
-import org.apache.commons.configuration2.builder.fluent.Configurations
-import org.apache.commons.configuration2.ex.ConfigurationException
-import org.litote.kmongo.eq
-import org.litote.kmongo.findOne
-import uk.ac.le.ember.labpipe.server.*
-import uk.ac.le.ember.labpipe.server.db.DatabaseUtil
-import uk.ac.le.ember.labpipe.server.notification.EmailUtil
-import uk.ac.le.ember.labpipe.server.services.*
+import mu.KotlinLogging
+import uk.ac.le.ember.labpipe.server.AuthManager
+import uk.ac.le.ember.labpipe.server.controllers.ConfigController
+import uk.ac.le.ember.labpipe.server.controllers.DatabaseController
+import uk.ac.le.ember.labpipe.server.controllers.EmailController
+import uk.ac.le.ember.labpipe.server.routes.RouteController
 import uk.ac.le.ember.labpipe.server.sessions.Runtime
-import java.io.File
-import java.nio.file.Paths
+import kotlin.system.exitProcess
 
+private val logger = KotlinLogging.logger {}
 
 fun startServer() {
-    Runtime.server = Javalin.create { config -> config.enableCorsForAllOrigins() }
+    Runtime.server = Javalin.create { config ->
+        config.enableCorsForAllOrigins()
+        config.enforceSsl = Runtime.config[ConfigController.Companion.LabPipeConfig.Security.enforceSsl]
+    }
     AuthManager.setManager()
-    generalRoutes()
-    parameterRoutes()
-    recordRoutes()
-    formRoutes()
-    queryRoutes()
-    manageRoutes()
-    uploadRoutes()
-    Runtime.server.start(Runtime.lpConfig.serverPort)
-    echo("Server running at " + Runtime.lpConfig.serverPort)
+    RouteController.addRoutes()
+    Runtime.server.start(Runtime.config[ConfigController.Companion.LabPipeConfig.port])
+    logger.info("Server running at ${Runtime.config[ConfigController.Companion.LabPipeConfig.port]}")
 }
 
 class LPServerCmdLine :
@@ -44,189 +34,36 @@ class LPServerCmdLine :
 
 }
 
-
-class Check : CliktCommand(name = "check", help = "Check database/email connection") {
+class Check : CliktCommand(name = "check", help = "Check database/email connection", treatUnknownOptionsAsArgs = true) {
     override fun run() {
-        importConfig()
-        DatabaseUtil.connect()
-        EmailUtil.connect()
-        EmailUtil.testConnection()
-        DatabaseUtil.testConnection()
+        Runtime.config = ConfigController.load()
+        EmailController.testConnection()
+        DatabaseController.testConnection()
+        exitProcess(0)
     }
 }
 
-class Run : CliktCommand(name = "run", help = "Run server") {
+class Run : CliktCommand(name = "run", help = "Run server", treatUnknownOptionsAsArgs = true) {
     private val debugMode by option("--debug", help = "debug mode").flag()
     // TODO enable daemon mode
 
     override fun run() {
         Runtime.debugMode = debugMode
-        importConfig()
-        DatabaseUtil.connect()
-        EmailUtil.connect()
-        EmailUtil.testConnection()
-        DatabaseUtil.testConnection()
+        Runtime.config = ConfigController.load()
+        DatabaseController.connect()
+        EmailController.connect()
+        Runtime.emailAvailable = EmailController.testConnection()
+        DatabaseController.testConnection()
         startServer()
     }
 }
 
-class Init : CliktCommand(name = "init", help = "Init server") {
+class Init : CliktCommand(name = "init", help = "Init server", treatUnknownOptionsAsArgs = true) {
 
     override fun run() {
-        importConfig()
-        DatabaseUtil.connect()
-        DatabaseUtil.testConnection()
-        runBlocking {
-            if (MONGO.COLLECTIONS.ROLES.findOne(OperatorRole::identifier eq DEFAULT_TOKEN_ROLE.identifier) == null) {
-                MONGO.COLLECTIONS.ROLES.insertOne(DEFAULT_TOKEN_ROLE)
-            }
-        }
-        runBlocking {
-            if (MONGO.COLLECTIONS.ROLES.findOne(OperatorRole::identifier eq DEFAULT_OPERATOR_ROLE.identifier) == null) {
-                MONGO.COLLECTIONS.ROLES.insertOne(DEFAULT_OPERATOR_ROLE)
-            }
-        }
-        runBlocking {
-            if (MONGO.COLLECTIONS.ROLES.findOne(OperatorRole::identifier eq DEFAULT_ADMIN_ROLE.identifier) == null) {
-                MONGO.COLLECTIONS.ROLES.insertOne(DEFAULT_ADMIN_ROLE)
-            }
-        }
-        runBlocking {
-            if (MONGO.COLLECTIONS.CLIENT_SETTINGS.findOne(ClientSettings::identifier eq DEFAULT_CLIENT_SETTING.identifier) == null) {
-                MONGO.COLLECTIONS.CLIENT_SETTINGS.insertOne(DEFAULT_CLIENT_SETTING)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.GENERAL.CONN_AUTH, roles = mutableSetOf(DEFAULT_OPERATOR_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.GENERAL.CONN_TOKEN, roles = mutableSetOf(DEFAULT_TOKEN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.PARAMETER.FROM_NAME, roles = mutableSetOf(DEFAULT_TOKEN_ROLE.identifier, DEFAULT_OPERATOR_ROLE.identifier, DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.FORM.ALL, roles = mutableSetOf(DEFAULT_TOKEN_ROLE.identifier, DEFAULT_OPERATOR_ROLE.identifier, DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.FORM.FROM_IDENTIFIER, roles = mutableSetOf(DEFAULT_TOKEN_ROLE.identifier, DEFAULT_OPERATOR_ROLE.identifier, DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.FORM.FROM_STUDY_INSTRUMENT, roles = mutableSetOf(DEFAULT_TOKEN_ROLE.identifier, DEFAULT_OPERATOR_ROLE.identifier, DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.MANAGE.CREATE.EMAIL_GROUP, roles = mutableSetOf(DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.MANAGE.CREATE.INSTRUMENT, roles = mutableSetOf(DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.MANAGE.CREATE.LOCATION, roles = mutableSetOf(DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.MANAGE.CREATE.OPERATOR, roles = mutableSetOf(DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.MANAGE.UPDATE.PASSWORD, roles = mutableSetOf(DEFAULT_ADMIN_ROLE.identifier, DEFAULT_OPERATOR_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.MANAGE.CREATE.ROLE, roles = mutableSetOf(DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.MANAGE.CREATE.STUDY, roles = mutableSetOf(DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.MANAGE.CREATE.TOKEN, roles = mutableSetOf(DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.QUERY.INSTRUMENT, roles = mutableSetOf(DEFAULT_TOKEN_ROLE.identifier, DEFAULT_OPERATOR_ROLE.identifier, DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.QUERY.INSTRUMENTS, roles = mutableSetOf(DEFAULT_TOKEN_ROLE.identifier, DEFAULT_OPERATOR_ROLE.identifier, DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.QUERY.RECORDS, roles = mutableSetOf(DEFAULT_TOKEN_ROLE.identifier, DEFAULT_OPERATOR_ROLE.identifier, DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.QUERY.STUDY_RECORDS, roles = mutableSetOf(DEFAULT_TOKEN_ROLE.identifier, DEFAULT_OPERATOR_ROLE.identifier, DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.QUERY.STUDY, roles = mutableSetOf(DEFAULT_TOKEN_ROLE.identifier, DEFAULT_OPERATOR_ROLE.identifier, DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.QUERY.STUDIES, roles = mutableSetOf(DEFAULT_TOKEN_ROLE.identifier, DEFAULT_OPERATOR_ROLE.identifier, DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.RECORD.ADD, roles = mutableSetOf(DEFAULT_TOKEN_ROLE.identifier, DEFAULT_OPERATOR_ROLE.identifier, DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
-        runBlocking {
-            val record = ApiAccessRole(url = API.UPLOAD.FORM_FILE, roles = mutableSetOf(DEFAULT_TOKEN_ROLE.identifier, DEFAULT_OPERATOR_ROLE.identifier, DEFAULT_ADMIN_ROLE.identifier))
-            if (MONGO.COLLECTIONS.API_ACCESS_ROLES.findOne(ApiAccessRole::url eq record.url) == null) {
-                MONGO.COLLECTIONS.API_ACCESS_ROLES.insertOne(record)
-            }
-        }
+        Runtime.config = ConfigController.load()
+        DatabaseController.connect()
+        DatabaseController.testConnection()
+        DatabaseController.init()
     }
 }
