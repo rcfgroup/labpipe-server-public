aec1414accc6798e88aac5533422549a9c89954d
Bo Zhao <bo.zhao@bz.dev>
2020-11-04 01:22:09 +0000

    Multiple changes
    1. Added default landing page
    2. APIs without access control are now public by default

--- src/main/kotlin/uk/ac/le/ember/labpipe/server/cmdline/CommandLineUtil.kt
+++ src/main/kotlin/uk/ac/le/ember/labpipe/server/cmdline/CommandLineUtil.kt
@@ -4,6 +4,12 @@ import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.option
 import io.javalin.Javalin
+import io.javalin.http.staticfiles.Location
+import io.javalin.plugin.openapi.OpenApiOptions
+import io.javalin.plugin.openapi.OpenApiPlugin
+import io.javalin.plugin.openapi.ui.ReDocOptions
+import io.javalin.plugin.openapi.ui.SwaggerOptions
+import io.swagger.v3.oas.models.info.Info
 import mu.KotlinLogging
 import uk.ac.le.ember.labpipe.server.AuthManager
 import uk.ac.le.ember.labpipe.server.controllers.ConfigController
@@ -15,10 +21,30 @@ import kotlin.system.exitProcess
 
 private val logger = KotlinLogging.logger {}
 
+private fun getOpenApiOptions(): OpenApiOptions {
+    val applicationInfo: Info = Info()
+        .version("1.0")
+        .description("LabPipe Server")
+    return OpenApiOptions(applicationInfo)
+        .path("/docs")
+        .swagger(SwaggerOptions("/swagger").title("LabPipe Swagger Documentation"))
+        .reDoc(ReDocOptions("/redoc").title("LabPipe ReDoc Documentation"))
+}
+
 fun startServer() {
     Runtime.server = Javalin.create { config ->
         config.enableCorsForAllOrigins()
         config.enforceSsl = Runtime.config[ConfigController.Companion.LabPipeConfig.Security.enforceSsl]
+        if (Runtime.config[ConfigController.Companion.LabPipeConfig.showBrowsableApi]) {
+            config.registerPlugin(OpenApiPlugin(getOpenApiOptions()))
+        }
+        if (Runtime.config[ConfigController.Companion.LabPipeConfig.showDefaultPage]) {
+            if (Runtime.config[ConfigController.Companion.LabPipeConfig.defaultPageDirectory].isEmpty()) {
+                config.addStaticFiles("/page")
+            } else {
+                config.addStaticFiles(Runtime.config[ConfigController.Companion.LabPipeConfig.defaultPageDirectory], Location.EXTERNAL)
+            }
+        }
     }
     AuthManager.setManager()
     RouteController.addRoutes()
